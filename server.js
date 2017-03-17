7ec44ea54703f55b6f507ed4faec7769fc2ca85a
Ben <bensass123@gmail.com>
2017-03-16 21:35:23 -0400

    final

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var path = require('path')
 
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
 // app.use('/static', express.static(path.join(__dirname, "\\app\\public")));
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/',require('./app/routing/htmlRoutes'));
 app.use('/',require('./app/routing/apiRoutes'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(PORT, function () {
 
   var host = server.address().address
   var PORT = server.address().PORT
